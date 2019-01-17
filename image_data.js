6fd906eb2a1d683fee011ffc90c3c99d33c32270
Youngji <36945452+youngji-koh@users.noreply.github.com>
2019-01-17 13:48:08 +0900

    Initial commit

---
+++ image_data.js
@@ -0,0 +1,10 @@
+var img = {
+    "images": [
+        {
+            "id": 139,
+            "width": 640,
+            "height": 426,
+            "file_name": "C:\Users\Admin\Brackets-workspace\A_starry_night.jpg",
+        }
+    ]
+}
